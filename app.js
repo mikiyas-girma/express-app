5328ed9df75113f78c4d3e79b76356bd80a9cf58
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-23 19:50:52 +0300

    passed data to views

--- app.js
+++ app.js
@@ -7,15 +7,23 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
   // res.sendFile('./views/index.html', { root:  __dirname});
   // using ejs view engine
-  res.render('index')
+
+  const blogs = [
+    { title: 'Blog 1', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'Blog 2', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'Blog 3', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+  ];
+  
+
+  res.render('index', { title: 'home', blogs})
 });
 
 app.get('/about', (req, res) => {
   // res.sendFile('./views/about.html', { root:  __dirname});
-  res.render('about')
+  res.render('about', { title: 'about'})
 });
 app.get('/blogs/create', (req, res) => {
-  res.render('create')
+  res.render('create', { title: 'create a blog'})
 });
 
 app.listen(3000);
@@ -23,7 +31,7 @@ app.listen(3000);
 // redirects
 app.get('/about-us', (req, res) => {
   // res.sendFile('./views/about.html', {root : __dirname})
-  res.render('about')
+  res.render('about', { title: 'about'})
 })
 
 // 404 page  

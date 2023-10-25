4ca4ab26ea12935ccf5a03d308fdb63b7b014530
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-25 11:34:41 +0300

    add trashcan icon

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -16,7 +16,7 @@ const blog_details = (req, res) => {
     .then((result) => {
       res.render('blogs/details', { blog : result, title: 'Blog details'} );
     })
-    .catch((err) => console.log(err));
+    .catch((err) => res.status(404).render('404', {title: 'Blog not found'}));
 };
 
 const blog_create_get = (req, res) => {
